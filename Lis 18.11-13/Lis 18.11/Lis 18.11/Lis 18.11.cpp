ccc21a71c6895057c7176ccd63613b472e859564
ArtemBoberok <129393819+ArtemBoberok@users.noreply.github.com>
2024-03-21 11:18:09 +0300

    Add files via upload

---
+++ Lis 18.11-13/Lis 18.11/Lis 18.11/Lis 18.11.cpp
@@ -0,0 +1,26 @@
+﻿// Листинг 18.11. Выход из блока __try с использованием инструкции __leave
+#include <windows.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	int* p = NULL; // пустой указатель на целое число
+	__try
+	{
+		if (!p)
+		{
+			cout << "Exit with __leave from the try block." << endl;
+			__leave; // выходим их блока
+		}
+		else
+		{
+			*p = 10;
+			cout << "*p = " << *p << endl;
+		}
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER)
+	{
+		cout << "There was some exception." << endl;
+	}
+	return 0;
+}
---
+++ Lis 18.11-13/Lis 18.13/Lis 18.13/Lis 18.13.cpp
@@ -0,0 +1,31 @@
+﻿// Листинг 18.13. Выброс структуры типа EXCEPTION_POINTERS как исключения языка программирования С++
+#include <windows.h>
+#include <iostream>
+#include <eh.h>
+using namespace std;
+void se_trans_func(unsigned code, EXCEPTION_POINTERS * info)
+{
+	EXCEPTION_RECORD er;
+	CONTEXT с;
+	EXCEPTION_POINTERS ep = { &er, &с };
+	er = *(info->ExceptionRecord);
+	с = *(info->ContextRecord);
+	throw ep;
+}
+int main()
+{
+	int a = 10, b = 0;
+	// устанавливаем функцию-транслятор
+	_set_se_translator(se_trans_func);
+	// перехватываем структурное исключение средствами С++
+	try
+	{
+		a /= b; // ошибка, деление на ноль
+	}
+	catch (EXCEPTION_POINTERS ep)
+	{
+		cout << "Exception code = " << hex
+			<< ep.ExceptionRecord->ExceptionCode << endl;
+	}
+	return 0;
+}
